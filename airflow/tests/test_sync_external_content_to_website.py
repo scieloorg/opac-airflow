6406a699f26b32e742b292a217f1eb20b585801d
Jamil Atta Junior <atta.jamil@gmail.com>
2020-02-10 14:59:30 -0300

    Task para sincronização do Press Releases

--- airflow/tests/test_sync_external_content_to_website.py
+++ airflow/tests/test_sync_external_content_to_website.py
@@ -1,12 +1,11 @@
 import os
-import json
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from opac_schema.v1 import models
 
 from .test_sync_kernel_to_website import load_json_fixture
-from dags.operations.sync_external_content_to_website_operations import NewsBuilder
+from dags.operations.sync_external_content_to_website_operations import NewsBuilder, PressReleaseBuilder
 
 FIXTURES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fixtures")
 
@@ -48,3 +47,36 @@ class TestNewsBuilder(unittest.TestCase):
         self.assertEqual("en", self.news_instance.language)
 
 
+class TestPressReleaseBuilder(unittest.TestCase):
+    def setUp(self):
+        self.press_release = load_json_fixture("rss-press-release-feed.json")
+        self.press_release_objects = patch(
+            "operations.sync_external_content_to_website_operations.models.PressRelease.objects"
+        )
+
+        self.new_journal = patch("operations.sync_external_content_to_website_operations.models.Journal")
+
+        PressReleaseObjectsMock = self.press_release_objects.start()
+        PressReleaseObjectsMock.get.side_effect = models.PressRelease.DoesNotExist
+
+        JournalObjectsMock = self.new_journal.start()
+        JournalObjectsMock._id = "J00001"
+        self.news_instance = PressReleaseBuilder(self.press_release[0], JournalObjectsMock, "pt_BR")
+
+    def tearDown(self):
+        self.press_release_objects.stop()
+
+    def test_press_release_instance_has_id(self):
+        self.assertIsNotNone(self.news_instance._id)
+
+    def test_press_release_instance_id_has_32_characters(self):
+        self.assertEqual(32, len(self.news_instance._id))
+
+    def test_press_release_instance_has_title(self):
+        self.assertEqual("Como os memes da internet conectam diferentes mundos?", self.news_instance.title)
+
+    def test_press_release_instance_has_description(self):
+        self.assertEqual("Que se destacou na internet e outras....", self.news_instance.content)
+
+    def test_press_release_instance_has_language(self):
+        self.assertEqual("pt_BR", self.news_instance.language)
