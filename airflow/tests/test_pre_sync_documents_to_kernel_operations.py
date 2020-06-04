66ca3536e2cd30c86f35afcc58b49e3d7e2dcf3c
Patricia Morimoto <patricia.morimoto@scielo.org>
2020-06-04 12:14:53 -0300

    Altera unittests para usar paths temporários ao invés de mocks

--- airflow/tests/test_pre_sync_documents_to_kernel_operations.py
+++ airflow/tests/test_pre_sync_documents_to_kernel_operations.py
@@ -1,4 +1,7 @@
 import tempfile
+import shutil
+import pathlib
+import zipfile
 from unittest import TestCase, main
 from unittest.mock import patch, MagicMock
 
@@ -9,63 +12,66 @@ from operations.pre_sync_documents_to_kernel_operations import get_sps_packages
 
 class TestGetSPSPackages(TestCase):
     def setUp(self):
+        self.xc_dir_name = tempfile.mkdtemp()
+        self.proc_dir_name = tempfile.mkdtemp()
         self.kwargs = {
-            "scilista_file_path": "dir/path/scilista.lst",
-            "xc_dir_name": "dir/source",
-            "proc_dir_name": "dir/destination",
+            "scilista_file_path": str(pathlib.Path(self.xc_dir_name) / "scilista.lst"),
+            "xc_dir_name": self.xc_dir_name,
+            "proc_dir_name": self.proc_dir_name,
         }
+        self.test_filepath = pathlib.Path(self.xc_dir_name) / "test.txt"
+        with self.test_filepath.open("w") as test_file:
+            test_file.write("Text test file")
 
-    @patch("operations.pre_sync_documents_to_kernel_operations.Path")
-    @patch("operations.pre_sync_documents_to_kernel_operations.open")
-    def test_get_sps_packages_creates_path_dirs(self, mk_open, MockPath):
-        get_sps_packages(**self.kwargs)
-        MockPath.assert_any_call(self.kwargs["xc_dir_name"])
-        MockPath.assert_any_call(self.kwargs["proc_dir_name"])
-
-    @patch("operations.pre_sync_documents_to_kernel_operations.open")
-    def test_read_scilista_from_file(self, mk_open):
-        get_sps_packages(**self.kwargs)
-        mk_open.assert_called_once_with("dir/path/scilista.lst")
+    def tearDown(self):
+        shutil.rmtree(self.xc_dir_name)
+        shutil.rmtree(self.proc_dir_name)
 
     @patch("operations.pre_sync_documents_to_kernel_operations.open")
     def test_get_sps_packages_raises_error_if_scilista_open_error(self, mk_open):
         mk_open.side_effect = FileNotFoundError
         self.assertRaises(FileNotFoundError, get_sps_packages, *self.kwargs)
 
-    @patch("operations.pre_sync_documents_to_kernel_operations.shutil")
-    @patch("operations.pre_sync_documents_to_kernel_operations.os.path.exists")
-    @patch("operations.pre_sync_documents_to_kernel_operations.open")
-    def test_get_sps_packages_moves_from_xc_dir_to_proc_dir(
-        self, mk_open, mk_path_exists, mk_shutil
-    ):
-        mk_path_exists.return_value = True
+    def test_get_sps_packages_moves_from_xc_dir_to_proc_dir(self):
         scilista_lines = ["rba v53n1", "rba 2019nahead", "rsp v10n4s1"]
-        mk_open.return_value.__enter__.return_value.readlines.return_value = (
-            scilista_lines
-        )
+        source_filenames = []
+        scilista_file_path = pathlib.Path(self.kwargs["scilista_file_path"])
+        with scilista_file_path.open("w") as scilista_file:
+            for line in scilista_lines:
+                scilista_file.write(line + "\n")
+                source_filenames.append("_".join(line.split()) + ".zip")
+        for filename in source_filenames:
+            zip_filename = pathlib.Path(self.xc_dir_name) / filename
+            with zipfile.ZipFile(zip_filename, "w") as zip_file:
+                zip_file.write(self.test_filepath)
 
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            self.kwargs["proc_dir_name"] = tmpdirname
-            get_sps_packages(**self.kwargs)
-            for scilista_line in scilista_lines:
-                with self.subTest(scilista_line=scilista_line):
-                    filename = "/{}_{}.zip".format(*scilista_line.split())
-                    mk_shutil.move.assert_any_call(
-                        self.kwargs["xc_dir_name"] + filename, tmpdirname + filename
-                    )
-
-    @patch("operations.pre_sync_documents_to_kernel_operations.shutil")
-    @patch("operations.pre_sync_documents_to_kernel_operations.os.path.exists")
-    @patch("operations.pre_sync_documents_to_kernel_operations.open")
-    def test_get_sps_packages_moves_anything_if_no_source_file(
-        self, mk_open, mk_path_exists, mk_shutil
-    ):
-        mk_path_exists.return_value = False
-        mk_open.return_value.__enter__.return_value.readlines.return_value = [
-            "rba v53n1"
-        ]
         get_sps_packages(**self.kwargs)
-        mk_shutil.move.assert_not_called()
+        for filename in source_filenames:
+            with self.subTest(filename=filename):
+                self.assertTrue(
+                    pathlib.Path(self.kwargs["proc_dir_name"])
+                    .joinpath(filename)
+                    .exists()
+                )
+
+    def test_get_sps_packages_moves_anything_if_no_source_file(self):
+        scilista_file_path = pathlib.Path(self.kwargs["scilista_file_path"])
+        package = "rba v53n2"
+        scilista_file_path.write_text(package)
+        source_filenames = [
+            "rba_v53n1", "rba_2019nahead", "rsp_v10n4s1"
+        ]
+        for filename in source_filenames:
+            zip_filename = pathlib.Path(self.xc_dir_name) / filename
+            with zipfile.ZipFile(zip_filename, "w") as zip_file:
+                zip_file.write(self.test_filepath)
+
+        get_sps_packages(**self.kwargs)
+        self.assertFalse(
+            pathlib.Path(self.kwargs["proc_dir_name"])
+            .joinpath("_".join(package.split()) + ".zip")
+            .exists()
+        )
 
 
 if __name__ == "__main__":
