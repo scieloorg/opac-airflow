a8f75aa085fb941bfd41ebbe3ed678a05ed9daf1
Roberta Takenaka <roberta.takenaka@scielo.org>
2020-10-09 14:12:08 -0300

    Caso configurado um email, será enviado o relatório PreSyncToKernel.log (#224)
    No assunto, apresenta quantos erros do total de itens da scilista
    Nota: Pode haver mais "erros" que o total de itens da scilista, pois 1 erro para cada arquivo zip não encontrado e um item na scilista tem pelo menos 1 arquivo zip

--- proc/CallPrepSyncToKernel.bat
+++ proc/CallPrepSyncToKernel.bat
@@ -1,8 +1,9 @@
 # Disponibiliza pacotes SPS resultantes do XML Converter + scilista + log
 # para o SciELO Publishing Framework 
 
-PREP_LOG=log/PrepSyncToKernel-$1.log
-./PrepSyncToKernel.bat $1 > $PREP_LOG
+ID_PROC=${1}
+PREP_LOG=log/PrepSyncToKernel-${ID_PROC}.log
+./PrepSyncToKernel.bat ${ID_PROC} > $PREP_LOG
 
 if [ -f SyncToKernel.ini ];
 then
@@ -12,4 +13,9 @@ fi
 if [ "" != "${XC_KERNEL_GATE}" ] && [ -e ${XC_KERNEL_GATE} ];
 then
     cp $PREP_LOG ${XC_KERNEL_GATE}
+fi
+
+if [ "" != "${EMAIL_TO}" ];
+then
+    mail -s "`cat /tmp/subject-${ID_PROC}.txt`" ${EMAIL_TO} < ${PREP_LOG}
 fi
