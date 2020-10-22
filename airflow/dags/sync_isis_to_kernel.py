b126f714cc23e45eed0671a781a361b628acdb40
Joffily Ferreira <joffily.ferreira@scielo.org>
2020-10-22 18:28:28 -0300

    Habilita tentativas de reexecução para a DAG sync_isis_to_kernel

--- airflow/dags/sync_isis_to_kernel.py
+++ airflow/dags/sync_isis_to_kernel.py
@@ -77,7 +77,7 @@ default_args = {
     "email": ["airflow@example.com"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 5,
     "retry_delay": timedelta(minutes=5),
 }
 
