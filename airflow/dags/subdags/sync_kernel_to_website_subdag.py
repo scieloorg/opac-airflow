0ed8d8336b7800e066f718995e8d55bb15435e7d
Roberta Takenaka <roberta.takenaka@scielo.org>
2020-12-16 08:57:57 -0300

    Corrige `finish()` porque não estava obtendo os `orphan_documents` e `orphan_renditions` resultantes das tarefas

--- airflow/dags/subdags/sync_kernel_to_website_subdag.py
+++ airflow/dags/subdags/sync_kernel_to_website_subdag.py
@@ -91,6 +91,7 @@ def create_subdag_to_register_documents_grouped_by_bundle(
             task_id=task_id,
             provide_context=True,
             python_callable=finish,
+            op_args=(groups.keys(), orphan_documents, orphan_renditions),
             dag=dag_subdag,
         )
 
@@ -145,44 +146,44 @@ def create_subdag_to_register_documents_grouped_by_bundle(
     return dag_subdag
 
 
-def finish(**kwargs):
+def finish(bundles, orphan_documents, orphan_renditions, **kwargs):
     Logger.info("Finish")
-    t1_orphan_documents = kwargs["ti"].xcom_pull(
-        key="orphan_documents", task_ids="t1") or []
-    t2_orphan_renditions = kwargs["ti"].xcom_pull(
-        key="orphan_renditions", task_ids="t2") or []
+
+    t1_orphan_documents = orphan_documents or []
+    for bundle_id in bundles:
+        t1_orphan_documents += kwargs["ti"].xcom_pull(
+            key="orphan_documents",
+            task_ids=f'register_documents_groups_id_{bundle_id}_docs') or []
+
+    t2_orphan_renditions = orphan_renditions or []
+    for bundle_id in bundles:
+        t2_orphan_renditions += kwargs["ti"].xcom_pull(
+            key="orphan_renditions",
+            task_ids=f'register_documents_groups_id_{bundle_id}_renditions') or []
+
     t3_orphan_renditions = kwargs["ti"].xcom_pull(
-        key="orphan_renditions", task_ids="t3") or []
+        key="orphan_renditions",
+        task_ids="register_documents_groups_id_renditions") or []
 
     if t1_orphan_documents:
         try:
-            orphan_documents = Variable.get(
-                "orphan_documents", [], deserialize_json=True)
-
-            orphan_documents += t1_orphan_documents
             Variable.set(
-                "orphan_documents", orphan_documents, serialize_json=True)
-            Logger.info("Finish %i orphan_documents", len(orphan_documents))
+                "orphan_documents", t1_orphan_documents, serialize_json=True)
         except Exception as e:
             # sqlalchemy.exc.OperationalError: (psycopg2.OperationalError)
             # could not connect to server: Connection refused
             Logger.info("%s", e)
+    Logger.info("Finish %i orphan_documents", len(t1_orphan_documents))
 
     if t2_orphan_renditions or t3_orphan_renditions:
         try:
-            orphan_renditions = Variable.get(
-                "orphan_renditions", [], deserialize_json=True)
-
-            orphan_renditions += t2_orphan_renditions + t3_orphan_renditions
+            orphan_renditions = t2_orphan_renditions + t3_orphan_renditions
             Variable.set(
                 "orphan_renditions", orphan_renditions, serialize_json=True)
-            Logger.info("Finish %i orphan_renditions", len(orphan_renditions))
         except Exception as e:
             # sqlalchemy.exc.OperationalError: (psycopg2.OperationalError)
             # could not connect to server: Connection refused
             Logger.info("%s", e)
-
+    Logger.info("Finish %i orphan_renditions", len(orphan_renditions))
     Logger.info("Finish - FIM")
     return True
-
-
