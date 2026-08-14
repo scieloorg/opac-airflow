da9293ba8548dcdc5fb42e863aa9680046cc987d
Roberta Takenaka <roberta.takenaka@scielo.org>
2026-08-14 11:49:55 -0300

    Atualiza com o que está em produção. O disparo para o airflow ocorre antes do término do gerapadrao

--- proc/GeraPadrao.bat
+++ proc/GeraPadrao.bat
@@ -17,7 +17,10 @@ echo
 GERAPADRAO_ID=$(date "+%Y-%m-%d-%H%M%s")
 nohup ./CallPrepSyncToKernel.bat ${GERAPADRAO_ID} > /tmp/CallPrepSyncToKernel.out 2>&1 &
 
+./PrepSyncToKernelCpBases.bat
+nohup ./CallTriggerSyncIsisToKernel.bat  ${GERAPADRAO_ID} > /tmp/CallTriggerSyncIsisToKernel.out 2>&1 &
+
 GeraScielo.bat .. .. log/GeraPadrao.log adiciona
 
-nohup ./CallTriggerSyncIsisToKernel.bat  ${GERAPADRAO_ID} > /tmp/CallTriggerSyncIsisToKernel.out 2>&1 &
-nohup ./CallGeraUriList.bat  ${GERAPADRAO_ID} > /tmp/CallGeraUriList.out 2>&1 &
+#nohup ./CallTriggerSyncIsisToKernel.bat  ${GERAPADRAO_ID} > /tmp/CallTriggerSyncIsisToKernel.out 2>&1 &
+#nohup ./CallGeraUriList.bat  ${GERAPADRAO_ID} > /tmp/CallGeraUriList.out 2>&1 &
